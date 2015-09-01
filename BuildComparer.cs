d7ce3b7573ecadd99c74a31affdf3fbf9e502474
frelun <fred@softresource.se>
2015-09-01 13:40:06 +0200

    initial commit

---
+++ BuildComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Deployer.Api.Models;
+
+namespace Deployer.Service
+{
+    public class BuildComparer : IEqualityComparer<Build>
+    {
+        public bool Equals(Build x, Build y)
+        {
+            return (x.ProjectId.Equals(y.ProjectId))
+                   && (x.ProjectName.Equals(y.ProjectName)
+                       && (x.StepName.Equals(y.StepName))
+                       && (x.Status.Equals(y.Status)));
+        }
+
+        public int GetHashCode(Build obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
